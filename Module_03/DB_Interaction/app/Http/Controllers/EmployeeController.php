8c84c483cfdd8ce6db195ca048f7e5c50110ac62
Artur Zelenco <my1lettersbox@yahoo.com>
2025-07-31 16:33:22 +0300

    feat: run php artisan migrate:fresh to add new columns

--- Module_03/DB_Interaction/app/Http/Controllers/EmployeeController.php
+++ Module_03/DB_Interaction/app/Http/Controllers/EmployeeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Employee; // подключаем модель
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
