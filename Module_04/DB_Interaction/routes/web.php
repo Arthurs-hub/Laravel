ff21aa9e83b98e3306b5b7a9d89c514a3114f965
Artur Zelenco <my1lettersbox@yahoo.com>
2025-08-01 16:41:00 +0300

    feat: edited web.php

--- Module_04/DB_Interaction/routes/web.php
+++ Module_04/DB_Interaction/routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('home', [
+    'name' => 'Иван Иванов',
+    'age' => 17,
+    'position' => 'Developer',
+    'address' => 'ул. Пушкина, д. 10',
+]));
 
-Route::get('/test_database', [EmployeeController::class, 'store']);
+Route::get('/contacts', fn() => view('contacts', [
+    'address' => 'ул. Ленина, д. 5',
+    'post_code' => '123456',
+    'email' => '',
+    'phone' => '+7 123 456 7890',
+]));
