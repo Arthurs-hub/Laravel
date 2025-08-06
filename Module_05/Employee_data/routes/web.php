42364fdab1a69728c4e0cedb8ce426c4517040fd
Artur Zelenco <my1lettersbox@yahoo.com>
2025-08-06 12:33:42 +0300

    feat: added rotes and configured VerifyCsrf Token.php

--- Module_05/Employee_data/routes/web.php
+++ Module_05/Employee_data/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('get-employee-data', [EmployeeController::class, 'index']);
+Route::post('store-form', [EmployeeController::class, 'store']);
+Route::put('/user/{id}', [EmployeeController::class, 'update']);
