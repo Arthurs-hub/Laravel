053bf433f38c6748b95e2733aeea0fadf300d2db
Artur Zelenco <my1lettersbox@yahoo.com>
2025-08-08 11:09:27 +0300

    feat: added model Employee.php file and updated EmployeeController.php

--- Module_05/Employee_data/app/Http/Controllers/EmployeeController.php
+++ Module_05/Employee_data/app/Http/Controllers/EmployeeController.php
@@ -3,113 +3,111 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    public function index()
+    public function index(): \Illuminate\View\View
     {
         return view('employee-form');
     }
 
-    public function store(Request $request)
+    public function store(Request $request): \Illuminate\Http\JsonResponse
     {
-
-        $name = $request->input('name');
-        $surname = $request->input('surname');
-        $position = $request->input('position');
-        $address = $request->input('address');
-        $email = $request->input('email');
-        $workData = $request->input('workData');
         $jsonDataRaw = $request->input('jsonData');
-
-        $jsonData = json_decode($jsonDataRaw, true);
+        $jsonData = json_decode($jsonDataRaw);
 
         $street = $city = $lat = $lng = null;
-        if (is_array($jsonData) && isset($jsonData[0]['address'])) {
-            $street = $jsonData[0]['address']['street'] ?? null;
-            $city = $jsonData[0]['address']['city'] ?? null;
-            $lat = $jsonData[0]['address']['geo']['lat'] ?? null;
-            $lng = $jsonData[0]['address']['geo']['lng'] ?? null;
+        if (is_array($jsonData) && isset($jsonData[0]->address)) {
+            $street = $jsonData[0]->address->street ?? null;
+            $city = $jsonData[0]->address->city ?? null;
+            $lat = $jsonData[0]->address->geo->lat ?? null;
+            $lng = $jsonData[0]->address->geo->lng ?? null;
         }
 
-        $path = $request->path();
-        $url = $request->url();
-
-        $id = rand(1, 1000);
+        $employee = new Employee([
+            'id' => rand(1, 1000),
+            'name' => $request->input('name'),
+            'surname' => $request->input('surname'),
+            'position' => $request->input('position'),
+            'address' => $request->input('address'),
+            'email' => $request->input('email'),
+            'workData' => $request->input('workData'),
+            'jsonData' => $jsonDataRaw,
+        ]);
 
         return response()->json([
-            'id' => $id,
-            'name' => $name,
-            'surname' => $surname,
-            'position' => $position,
-            'address' => $address,
-            'email' => $email,
-            'workData' => $workData,
+            'id' => $employee->id,
+            'name' => $employee->name,
+            'surname' => $employee->surname,
+            'position' => $employee->position,
+            'address' => $employee->address,
+            'email' => $employee->email,
+            'workData' => $employee->workData,
             'jsonData' => $jsonData,
             'street' => $street,
             'city' => $city,
             'lat' => $lat,
             'lng' => $lng,
-            'path' => $path,
-            'url' => $url,
+            'path' => $request->path(),
+            'url' => $request->url(),
         ]);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id): \Illuminate\Http\JsonResponse
     {
+        $employeeId = $id;
+        $employee = Employee::findOrFail($employeeId);
 
-        $name = $request->input('name');
-        $surname = $request->input('surname');
-        $position = $request->input('position');
-        $address = $request->input('address');
-        $email = $request->input('email');
-        $workData = $request->input('workData');
         $jsonDataRaw = $request->input('jsonData');
-
-        $jsonData = json_decode($jsonDataRaw, true);
+        $jsonData = json_decode($jsonDataRaw);
 
         $street = $city = $lat = $lng = null;
-        if (is_array($jsonData) && isset($jsonData[0]['address'])) {
-            $street = $jsonData[0]['address']['street'] ?? null;
-            $city = $jsonData[0]['address']['city'] ?? null;
-            $lat = $jsonData[0]['address']['geo']['lat'] ?? null;
-            $lng = $jsonData[0]['address']['geo']['lng'] ?? null;
+        if (is_array($jsonData) && isset($jsonData[0]->address)) {
+            $street = $jsonData[0]->address->street ?? null;
+            $city = $jsonData[0]->address->city ?? null;
+            $lat = $jsonData[0]->address->geo->lat ?? null;
+            $lng = $jsonData[0]->address->geo->lng ?? null;
         }
 
-        $path = $request->path();
-        $url = $request->url();
+        $employee->name = $request->input('name');
+        $employee->surname = $request->input('surname');
+        $employee->position = $request->input('position');
+        $employee->address = $request->input('address');
+        $employee->email = $request->input('email');
+        $employee->workData = $request->input('workData');
+        $employee->jsonData = $jsonDataRaw;
 
         return response()->json([
-            'id' => $id,
-            'name' => $name,
-            'surname' => $surname,
-            'position' => $position,
-            'address' => $address,
-            'email' => $email,
-            'workData' => $workData,
+            'id' => $employee->id,
+            'name' => $employee->name,
+            'surname' => $employee->surname,
+            'position' => $employee->position,
+            'address' => $employee->address,
+            'email' => $employee->email,
+            'workData' => $employee->workData,
             'jsonData' => $jsonData,
             'street' => $street,
             'city' => $city,
             'lat' => $lat,
             'lng' => $lng,
-            'path' => $path,
-            'url' => $url,
+            'path' => $request->path(),
+            'url' => $request->url(),
         ]);
     }
 
-    public function getPath(Request $request)
+    public function getPath(Request $request): string
     {
         return $request->path();
     }
 
-    public function getUrl(Request $request)
+    public function getUrl(Request $request): string
     {
         return $request->url();
     }
 
-    public function getEmployeeData(Request $request)
+    public function getEmployeeData(Request $request): \Illuminate\Http\JsonResponse
     {
-
         $data = $request->only(['name', 'surname', 'position', 'address', 'email', 'workData', 'jsonData']);
 
         return response()->json([
