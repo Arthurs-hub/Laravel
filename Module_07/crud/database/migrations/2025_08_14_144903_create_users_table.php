8bbf67f324d5cf662f689a05e0a5ce65e43021d3
Artur Zelenco <my1lettersbox@yahoo.com>
2025-08-14 17:56:06 +0300

    feat: run artisan migration

--- Module_07/crud/database/migrations/2025_08_14_144903_create_users_table.php
+++ Module_07/crud/database/migrations/2025_08_14_144903_create_users_table.php
@@ -9,10 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
@@ -22,6 +26,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 };
