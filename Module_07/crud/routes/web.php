831732f53d285da8ccc4171f134790bb014a1fda
Artur Zelenco <my1lettersbox@yahoo.com>
2025-08-14 17:57:39 +0300

    feat: created all routes

--- Module_07/crud/routes/web.php
+++ Module_07/crud/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PdfGeneratorController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/users', [UserController::class, 'index']); 
+Route::get('/users/{id}', [UserController::class, 'get']); 
+Route::post('/users', [UserController::class, 'store']); 
+Route::get('/resume/{id}', [PdfGeneratorController::class, 'index']); 
+
+Route::get('/employee-form', function () {
+    return view('employee_form');
 });
