6e6160fd25293f474e951c985106a5436149cdbb
Artur Zelenco <my1lettersbox@yahoo.com>
2025-09-05 13:14:11 +0300

    feat: created Kernel.php and configured bootstrap/app.php as task scheduler in laravel 12

--- Module_10/ManageProducts/bootstrap/app.php
+++ Module_10/ManageProducts/bootstrap/app.php
@@ -1,13 +1,17 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Jobs\ClearCache;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -15,4 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->job(new ClearCache)->everyMinute();
     })->create();
