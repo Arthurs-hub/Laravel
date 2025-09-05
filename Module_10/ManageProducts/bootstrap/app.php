725836a89890db8155ed654ce144805f8ec56d45
Artur Zelenco <my1lettersbox@yahoo.com>
2025-09-05 13:37:38 +0300

    feat: run artisan queue:listen and php artisan schedule:work for everyMinute and hourly live successfuly

--- Module_10/ManageProducts/bootstrap/app.php
+++ Module_10/ManageProducts/bootstrap/app.php
@@ -15,11 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        
     })
     ->withSchedule(function (Schedule $schedule) {
-        $schedule->job(new ClearCache)->everyMinute();
-    })->create();
+
+        $schedule->job(new ClearCache)->hourly();
+    })
+    ->create();
