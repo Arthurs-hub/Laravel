e97cabf58ff4debb76d942e2808a678da839d6e6
Artur Zelenco <my1lettersbox@yahoo.com>
2025-09-12 15:14:30 +0300

    feat: created UserRegistered and UserRegisteredListener with functions

---
+++ Module_12/users_auth/app/Listeners/UserRegisteredListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use App\Mail\Welcome;
+use Illuminate\Support\Facades\Mail;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class UserRegisteredListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserRegistered $event): void
+    {
+
+        Mail::to($event->user->email)->send(new Welcome($event->user));
+
+        $text = "Зарегистрирован новый пользователь:\n<b>Имя:</b> {$event->user->name}\n<b>Email:</b> {$event->user->email}";
+        Telegram::sendMessage([
+            'chat_id' => env('TELEGRAM_CHANNEL_ID', ''),
+            'parse_mode' => 'html',
+            'text' => $text
+        ]);
+    }
+}
