4400718e160dcdc3992d44cd0eac5abbd766c803
Artur Zelenco <my1lettersbox@yahoo.com>
2025-09-19 11:51:32 +0300

    feat: edited User.php and web.php

--- Module_14/admin_panel/app/Models/User.php
+++ Module_14/admin_panel/app/Models/User.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use TCG\Voyager\Models\User as VoyagerUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends VoyagerUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
